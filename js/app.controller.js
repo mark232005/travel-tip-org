d7968d4e1bf1fe587537c36a62b9a4ed713b6dbe
Dima <dima.aluf@gmail.com>
2025-01-22 21:22:38 +0200

    fix

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,7 +1,7 @@
 import { utilService } from './services/util.service.js'
 import { locService } from './services/loc.service.js'
 import { mapService } from './services/map.service.js'
-let gUserPos = 'hello'
+let gUserPos = null
 console.log('gUserPos:', gUserPos)
 window.onload = onInit
 
